cabf19df2d3e2ef11afe9d8e4e085035b6b24605
mlwebdev <mlwebdeveloper@hotmail.com>
2017-11-19 21:03:25 -0500

    Configure first intergration test for voter component
    Configure first intergration test for voter component

--- tdd-angular-integrated/src/app/1-voter/voter.component.spec.ts
+++ tdd-angular-integrated/src/app/1-voter/voter.component.spec.ts
@@ -1,8 +1,16 @@
 import { VoterComponent } from './voter.component';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 
 describe('VoterComponent', () => {
+  let component: VoterComponent;
+  let fixture: ComponentFixture<VoterComponent>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VoterComponent ]
+    });
+    fixture = TestBed.createComponent(VoterComponent);
+    component = fixture.componentInstance;
   });
 
   it('', () => {
