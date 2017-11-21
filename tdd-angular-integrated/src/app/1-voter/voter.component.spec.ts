e58cc3d8fdfc2885496cb0e188669dd1349d50fa
mlwebdev <mlwebdeveloper@hotmail.com>
2017-11-21 08:58:33 -0500

    Integration test - css
    Integration test - css

--- tdd-angular-integrated/src/app/1-voter/voter.component.spec.ts
+++ tdd-angular-integrated/src/app/1-voter/voter.component.spec.ts
@@ -1,18 +1,31 @@
 import { VoterComponent } from './voter.component';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
 
 describe('VoterComponent', () => {
   let component: VoterComponent;
   let fixture: ComponentFixture<VoterComponent>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
+    TestBed.configureTestingModule ({
       declarations: [ VoterComponent ]
     });
-    fixture = TestBed.createComponent(VoterComponent);
-    component = fixture.componentInstance;
   });
 
-  it('', () => {
+  it('should render total votes', () => {
+    fixture = TestBed.createComponent(VoterComponent);
+    fixture.detectChanges();
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.vote-count'));
+    let el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain(21);
   });
 });
